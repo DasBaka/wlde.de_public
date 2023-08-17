d7d4fe34229d6abc04635531fc46a06ddee36b49
DasBaka <122975603+DasBaka@users.noreply.github.com>
2023-08-17 16:34:19 +0200

    basic structure & basic frontpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontpageComponent } from './main/frontpage/frontpage.component';
+import { RestaurantsComponent } from './main/restaurants/restaurants.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'frontpage',
+    component: FrontpageComponent,
+  },
+  {
+    path: 'restaurants',
+    component: RestaurantsComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'frontpage',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FrontpageComponent } from './main/frontpage/frontpage.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { RestaurantsComponent } from './main/restaurants/restaurants.component';
+import { AccountComponent } from './main/account/account.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { ImprintComponent } from './main/imprint/imprint.component';
+import { PoliciesComponent } from './main/policies/policies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FrontpageComponent,
+    RestaurantsComponent,
+    AccountComponent,
+    ContactComponent,
+    ImprintComponent,
+    PoliciesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
